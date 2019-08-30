5e05121371058cdd0f22ebaf463fa0845172a5cc
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2019-08-30 14:47:08 +0430

    Add annotation ComponentScan for scan

---
+++ src/main/java/com/company/guruspring/config/Config.java
@@ -0,0 +1,9 @@
+package com.company.guruspring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.company.guruspring"})
+public class Config {
+}
